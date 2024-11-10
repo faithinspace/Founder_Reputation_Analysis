19bb9a59f9134bbd0dece9d8d2e4c4115074276e
faithinspace <153125664+faithinspace@users.noreply.github.com>
2024-11-10 13:55:18 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -56,18 +56,18 @@ def setup_selenium(attended_mode=False):
     options = Options()
     service = Service(ChromeDriverManager().install())
 
-    # Apply headless options based on whether the code is running in Docker
-    if is_running_in_docker():
-        # Running inside Docker, use Docker-specific headless options
-        for option in HEADLESS_OPTIONS_DOCKER:
-            options.add_argument(option)
-    else:
-        # Not running inside Docker, use the normal headless options
-        for option in HEADLESS_OPTIONS:
-            options.add_argument(option)
-
+    # # Apply headless options based on whether the code is running in Docker
+    # if is_running_in_docker():
+    #     # Running inside Docker, use Docker-specific headless options
+    #     for option in HEADLESS_OPTIONS_DOCKER:
+    #         options.add_argument(option)
+    # else:
+    #     # Not running inside Docker, use the normal headless options
+    #     for option in HEADLESS_OPTIONS:
+    #         options.add_argument(option)
+    options.add_argument("--headless=new")
     # Initialize the WebDriver
-    driver = webdriver.Chrome(service=service)
+    driver = webdriver.Chrome(service=service, options=options)
     return driver
 
 
